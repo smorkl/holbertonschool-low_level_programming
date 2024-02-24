6d785573f2b0bbf1d6b0112e953cfbcef063c02b
smorkl <smsalazar0319@gmail.com>
2024-02-24 23:36:19 +0000

    create

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this code prints all single digit numbers of
+ *        base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int numero = 48;
+
+	while (numero < 58)
+		{
+			putchar(numero);
+			numero++;
+		}
+	putchar('\n');
+	return (0);
+}
