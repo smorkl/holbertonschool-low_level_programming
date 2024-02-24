f2e309586e1045d79e79b244efa08066c545df29
smorkl <smsalazar0319@gmail.com>
2024-02-24 01:41:06 +0000

    create

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-   /*
-    this program prints programming is like bilding a multilingual
-	puzzle and return a 0
-   */
+/*
+ *this program prints programming is like bilding a multilingual
+ *puzzle and return a 0
+ */
 
-   int main()
-   {
-	   puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
-	   return (0);
-   }
+	return (0);
+}
