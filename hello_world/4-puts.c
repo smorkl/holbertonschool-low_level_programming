aefa90874284c407a2363c6cf6c0fe849615a8cc
smorkl <smsalazar0319@gmail.com>
2024-02-24 02:04:50 +0000

    modificar

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /*
- *main - this program prints programming is like bilding a multilingual puzzle
+ *main - prints "Programming is like building a multilingual puzzle.
  *
- *Return: Always 0
+ *Return: Always 0.
  */
 
 int main(void)
