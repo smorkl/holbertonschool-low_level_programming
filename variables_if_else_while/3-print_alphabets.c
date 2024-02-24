9dea47afd6211540f6295b72f26bbcac7e0ff638
smorkl <smsalazar0319@gmail.com>
2024-02-24 22:20:06 +0000

    modificacion

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,29 @@
-3
+#include <stdio.h>
+
+/**
+ * main - this code shows me the english alphabet
+ *          in lowercase and capital.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int codigo_mayusculas = 65;
+	int codigo_minusculas = 97;
+
+	while (codigo_mayusculas != 90 && codigo_minusculas != 122)
+	{
+		if (codigo_mayuscula
+		putchar(codigo_mayusculas);
+		codigo_mayusculas++;
+
+		putchar(codigo_minusculas);
+		codigo_minusculas++;
+
+		putchar('\n');
+
+	}
+
+	return (0);
+}
