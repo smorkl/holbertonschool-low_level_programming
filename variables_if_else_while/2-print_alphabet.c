710bed49bd4cc846e26f1a6ad3bce21ac82c956b
smorkl <smsalazar0319@gmail.com>
2024-02-24 21:32:06 +0000

    modificar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,22 @@
-2
+#include <stdio.h>
+
+/**
+ * main - this code shows me the english alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int codigo_letra = 97;
+
+	while (codigo_letra != 123)
+	{
+		putchar(codigo_letra);
+		codigo_letra++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
