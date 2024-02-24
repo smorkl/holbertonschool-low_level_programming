97aab0ec3e908e8ee30bc9152b5de61ff9c5b2b9
smorkl <smsalazar0319@gmail.com>
2024-02-24 22:35:16 +0000

    modificacion

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,12 @@ int main(void)
 	int codigo_mayusculas = 65;
 	int codigo_minusculas = 97;
 
-	while (codigo_minusculas != 122)
+	while (codigo_minusculas != 123)
 	{
 		putchar(codigo_minusculas);
 		codigo_minusculas++;
 	}
-	while (codigo_mayusculas != 90)
+	while (codigo_mayusculas != 91)
 	{
 		putchar(codigo_mayusculas);
 		codigo_mayusculas++;
