4f7c70dc93a8deadb1122ffed37c308d85274932
smorkl <smsalazar0319@gmail.com>
2024-02-24 23:01:58 +0000

    modificacion

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,27 @@
-4
+#include <stdio.h>
+
+/**
+ * main - this code shows me the english alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int codigo_letra = 97;
+
+	while (codigo_letra != 123)
+	{
+		if (codigo_letra == 113 || codigo_letra == 101)
+		{
+			codigo_letra++;
+			continue;
+		}
+		putchar(codigo_letra);
+		codigo_letra++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
