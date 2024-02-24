aea9549537d187b263761ee113323888a73fba3d
smorkl <smsalazar0319@gmail.com>
2024-02-24 02:44:05 +0000

    create

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *main - print "with proper grammar, but the outcome is a piece of art,"
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
