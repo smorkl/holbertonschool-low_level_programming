af23d51d5ce1ca9e6a6395ce08bb78d948b130c4
smorkl <smsalazar0319@gmail.com>
2024-02-25 00:00:58 +0000

    create}

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - this code prints all single digit numbers of
+ *        base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int numero = 48;
+	int letra_minuscula = 97;
+
+	while (numero < 58)
+	{
+		putchar(numero);
+		numero++;
+	}
+	while (letra_minuscula < 103)
+	{
+		putchar(letra_minuscula);
+		letra_minuscula++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
