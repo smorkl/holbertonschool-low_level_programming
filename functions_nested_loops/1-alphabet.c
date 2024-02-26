87ba3525317d3a4c34a68783fa8395bf6e8d7309
smorkl <smsalazar0319@gmail.com>
2024-02-26 00:10:18 +0000

    create

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This function prints the lowercase alphabet from 'a' to 'z'.
+ */
+
+void print_alphabet(void)
+{
+    int codigo_letra = 97;
+
+    while (codigo_letra < 123)
+    {
+        _putchar(codigo_letra);
+        codigo_letra++;
+    }
+
+    _putchar('\n');
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
