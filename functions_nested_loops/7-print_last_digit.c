652447a7b0d26e3432dcfd2d1ca01a18ffc06441
smorkl <smsalazar0319@gmail.com>
2024-02-28 23:31:03 +0000

    create

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -20,15 +20,3 @@ int print_last_digit(int num)
 
 	return (num_ultime);
 }
-
-int main(void)
-{
-	int r;
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
-}
