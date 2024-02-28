7c66e41a7037ce9015d21993772ba4e4078a23ee
smorkl <smsalazar0319@gmail.com>
2024-02-28 20:23:33 +0000

    create

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - This function checks whether a character or a letter
  * @c: character
  *
- * Return : 1 if is a letter o 0 if a character
+ * Return: 1 if is a letter o 0 if a character
  */
 
 int _isalpha(int c)
