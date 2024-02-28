9cf2a591611e851160a4f4cbc981a7c888ec779b
smorkl <smsalazar0319@gmail.com>
2024-02-28 20:21:48 +0000

    create

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - This function checks whether a character or a letter
+ * @c: character
+ *
+ * Return : 1 if is a letter o 0 if a character
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
