23bf38830af3c4e5c66e35ef59c6da9b2e96c3d2
smorkl <smsalazar0319@gmail.com>
2024-02-28 21:29:27 +0000

    modificar

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Calculates the absolute value of an integer.
+ * @int: number
+ *
+ * Rteurn: - if is negative o + if is postive
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return(-num);
+	}
+	else
+	{
+		return(num);
+	}
+}
