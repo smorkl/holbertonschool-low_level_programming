ee6fc6e5e1aadd8f6393f75c3e75bcdd79d50c61
smorkl <smsalazar0319@gmail.com>
2024-02-29 01:59:24 +0000

    modificar

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int numero_multiplicador = 0;
+	int numero_factor = 0;
+
+	while (numero_multiplicador < 10)
+	{
+		if (numero_factor < 11)
+		{
+			int resultado = numero_multiplicador * numero_factor;
+
+			if (resultado < 10)
+			{
+				_putchar('0' + resultado);
+				_putchar(',');
+				_putchar(' ');
+				numero_factor++;
+			}
+			else
+			{
+				int primero;
+				int segundo;
+
+				primero = resultado / 10;
+				_putchar('0' + primero);
+				segundo = resultado % 10;
+				_putchar('0' + segundo);
+				_putchar(',');
+				_putchar(' ');
+				numero_factor++;
+			}
+			if (numero_factor == 11)
+			{
+				numero_factor = 0;
+				_putchar('\n');
+				numero_multiplicador++;
+			}
+		}
+	}
+}
