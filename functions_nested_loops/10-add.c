5d66458130efc160c813b85f187e1d9f517f6850
smorkl <smsalazar0319@gmail.com>
2024-02-29 23:10:06 +0000

    create

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - suma 2 numeros
+ * @n1: number one
+ * @n2: number two
+ *
+ * Return: resultado
+ */
+
+int add(int n1, int n2)
+{
+	int resultado;
+
+	resultado = n1 + n2;
+	return (resultado);
+}
