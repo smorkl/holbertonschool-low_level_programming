02b3f5ca1c274455ec324ea8d88c7ff00f97827d
smorkl <smsalazar0319@gmail.com>
2024-03-01 02:39:18 +0000

    create

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * more_numbers - print the numbers from 0 to 14
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int repetidor = 0;
+
+	while (repetidor < 11)
+	{
+		int numero = 0;
+
+		if (numero < 10)
+		{
+			_putchar('0' + numero);
+			numero++;
+		}
+		else
+		{
+			int primero;
+			int segundo;
+
+			primero = resultado / 10;
+			_putchar('0' + primero);
+			segundo = resultado % 10;
+			_putchar('0' + segundo);
+			numero++;
+		}
+		_putchar('\n');
+		repetidor++;
+	}
+}
