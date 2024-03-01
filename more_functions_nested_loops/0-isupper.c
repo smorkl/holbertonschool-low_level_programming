6a092ba872169b5e2f8eebbf768a5a320a54ed83
smorkl <smsalazar0319@gmail.com>
2024-03-01 01:20:51 +0000

    modificar

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - This function tells me if a letter is lowercase or uppercase
+ *@c: lether
+ *
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= A && c <= Z)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 C - More functions, more nested loops
   	0-isupper.c
+		use if and I put the condition that if it is uppercase to return 1 and else to return 0
