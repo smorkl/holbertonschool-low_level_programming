eb41b090bdc7ff011fe153e43ca4448cdd8eb58d
smorkl <smsalazar0319@gmail.com>
2024-03-02 23:58:06 +0000

    create

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void print_triangle(int size)
+{
+	int espacios;
+	int size2;
+
+	size2 = size;
+
+	for (espacios = 0; espacios < size2; espacios++)
+	{
+		_putchar(' ');
+	}
+}
