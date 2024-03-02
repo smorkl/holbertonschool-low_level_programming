f2411fef4000d5daa03dec59fbeeb29451d3767c
smorkl <smsalazar0319@gmail.com>
2024-03-02 20:27:41 +0000

    create

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_square(int size)
+{
+	int contador1 = 0;
+
+	while (contador1 < size)
+	{
+		for(int contador2 = 0; contador2 < n; contador2++)
+		{
+			_putchar('#')
+		}
+		contador1++;
+		_putchar('\n');
+	}
+}
