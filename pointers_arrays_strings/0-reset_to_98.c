0f45f1b5e2e97989845ca70b0785e310191466e4
smorkl <smsalazar0319@gmail.com>
2024-03-03 04:42:11 +0000

    create

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -9,14 +9,5 @@
 
 void reset_to_98(int *n)
 {
-	int primero;
-	int segundo;
 	*n = 98;
-
-	primero = *n / 10;
-	_putchar('0' + primero);
-	segundo = *n % 10;
-	_putchar('0' + segundo);
-
-	_putchar('\n');
 }
