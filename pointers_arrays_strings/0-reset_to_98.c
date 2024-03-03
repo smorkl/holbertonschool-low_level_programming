444ac296dd03c78247c43bc04f19f8ad11a00659
smorkl <smsalazar0319@gmail.com>
2024-03-03 04:23:28 +0000

    modificar

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	int p = 98;
+	int primero;
+	int segundo;
+
+	n = &p;
+
+	primero = n / 10;
+	_putchar('0' + primero);
+	segundo = n % 10;
+	_putchar('0' + segundo);
+
+	_putchar('\n')
+}
