5514600bcaade0f44815152d92da0b60ae94e9b8
smorkl <smsalazar0319@gmail.com>
2024-03-03 04:58:41 +0000

    modificar

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - return
+ *
+ * @n: the
+ *
+ * Return: the longitud
+ */
+
+int _strlen(char *s)
+{
+	int longitud = 0;
+
+	while (*s != '\0')
+	{
+		longitud++;
+		s++;
+	}
+	return longitud;
+}
