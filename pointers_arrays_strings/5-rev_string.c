100bc1193dc9e548f55f7bb701fc062b4fea07e7
smorkl <smsalazar0319@gmail.com>
2024-03-05 15:15:20 -0500

    creatre

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - the function prints every other character of a string.
+ * @str: matriz of number.
+ */
+
+void puts2(char *str)
+{
+	int cantidad = 0;
+	int resultado;
+
+	while (str[cantidad] != '\0')
+	{
+		cantidad++;
+	}
+	
+	resultado = cantidad - 1;	
+	
+	int i;
+	
+	for (i = 0; i < resultado; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n')
+}
