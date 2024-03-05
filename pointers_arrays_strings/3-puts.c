46ae3962da52e22333f79d81af514f43e0a448f9
root <root@LAPTOP-Q3JQP06D>
2024-03-05 11:18:19 -0500

    create

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _Puts - the funcion print the string of chars.
+ * @str: matriz of chars.
+ *
+ */
+
+void _puts(char *str)
+{
+	int a;
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n')
+}
