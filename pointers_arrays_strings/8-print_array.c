05ab79362a3cf46e0c38c1e603497a57ea30800e
smorkl <smsalazar0319@gmail.com>
2024-03-06 18:26:12 -0500

    create

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - the fuction print the number followed by a comma
+ * @a: array
+ * @n: the number of elements of the array
+ */
+
+void print_array(int *a, int n);
+{
+	int contador = 0;
+
+	while (contador < n)
+	{
+		if (contador < (n - 1))
+		{
+			printf("%d, " a[contador]);
+			contador++;
+		}
+		else
+		{
+			printf("%d" a[contador]);
+			contador++;
+		}
+	}
+	_putchar("\n");
+}
