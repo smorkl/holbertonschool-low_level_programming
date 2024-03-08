5fc1374a05509bcf5c93a7468a3a6cbae7f7ec0f
smorkl <smsalazar0319@gmail.com>
2024-03-07 20:03:05 -0500

    create

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * *_strcat - the function concatenates two strings.
+ * @dest: string 1
+ * @src: string 2
+ *
+ * Return: pointer of string concatenate
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int contador__dest = 0;
+	int contador__src = 0;
+	int cantidad_char;
+	char tmp;
+	int i = 0;
+
+	while (src[contador_src] != '\0')
+	{
+		contador_src++;
+	}
+
+	while (dest[contador_dest] != '\0')
+	{
+		contador_dest++;
+	}
+
+	cantidad_char = contador_src + contador_dest;
+
+	for (contador_dest; contador_dest < cantidad_char; contador_dest++)
+	{
+		tmp = src[i];
+		dest[contador_dest] = tmp;
+		i++;
+	}
+	return (*dest);
+}
