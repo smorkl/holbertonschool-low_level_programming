b61585bb251fca2e9d3898483ca7950e370c2c26
smorkl <smsalazar0319@gmail.com>
2024-03-13 16:18:42 -0500

    ceate

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int contador;
 
-	str = (char*)malloc(size * sizeof(char));
+	str = malloc(size * sizeof(char));
 
 	if (str == NULL)
 	{
