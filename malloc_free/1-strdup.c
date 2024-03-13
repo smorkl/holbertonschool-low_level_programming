38fa8c9e77a6ae4fef79e6f9a022b280afb96ffe
smorkl <smsalazar0319@gmail.com>
2024-03-13 18:39:36 -0500

    create

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - the function copie a string of char
+ * @str: string of char
+ *
+ * Return: direction of pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *deposito;
+	int bytes = o;
+	int contador;
+
+	while (str[bytes])
+	{
+		bytes++;
+	}
+
+	deposito = malloc(sizeof(char) * bytes);
+
+	if (deposito == NULL)
+	{
+		return (NULL);
+	}
+
+	while (contador < bytes)
+	{
+		deposito[contador] = str[contador];
+	}
+
+	return (deposito);
+}
