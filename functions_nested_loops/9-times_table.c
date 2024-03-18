b7a0035ab828a68458782bc62a85fcae56849bc4
smorkl <smsalazar0319@gmail.com>
2024-03-18 17:35:26 -0500

    modificar

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,28 +5,33 @@
  */
 void times_table(void)
 {
-    int multiplicador, factor, resultado;
+	int  multiplicador, factor, resultado;
 
-    for (multiplicador = 0; multiplicador <= 9; multiplicador++)
-    {
-        for (factor = 0; factor <= 9; factor++)
-        {
-            resultado = multiplicador * factor;
-            if (factor > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            if (resultado < 10 && factor > 0)
-            {
-                _putchar(' ');
-            }
-            if (resultado >= 10)
-            {
-                _putchar(resultado / 10 + '0');
-            }
-            _putchar(resultado % 10 + '0');
-        }
-        _putchar('\n');
-    }
+	for (multiplicador = 0; multiplicador <= 9; multiplicador++)
+	{
+		for (factor = 0; factor <= 9; factor++)
+		{
+			resultado = multiplicador * factor;
+
+			if (factor > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (resultado < 10 && factor > 0)
+			{
+				_putchar(' ');
+			}
+
+			if (resultado >= 10)
+			{
+				_putchar(resultado / 10 + '0');
+			}
+
+			_putchar(resultado % 10 + '0');
+		}
+
+		__putchar('\n');
+	}
 }
