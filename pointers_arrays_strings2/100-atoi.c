83b22cdd5d564518d8a2e37164aed17623aa1e4d
smorkl <smsalazar0319@gmail.com>
2024-03-18 18:22:48 -0500

    modificar

---
+++ pointers_arrays_strings2/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - the fuction convert a string to an integer.
+ * @s: the string
+ *
+ * Return: a int wint signo
+ */
+
+int _atoi(char *s)
+{
+	int señal = 1;
+	unsigned int num = 0;
+
+	while (*s == ' ')
+	{
+		s++;
+	}
+
+	if (*s == '-')
+	{
+		señal = -1;
+		s++,
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		num = (num * 10) + (*s - '0');
+		s++;
+	}
+
+	return (num * señal)
+}
