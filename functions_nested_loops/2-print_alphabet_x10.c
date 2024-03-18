e6da015fb1720455277883098ab23313baf520a5
smorkl <smsalazar0319@gmail.com>
2024-03-18 16:34:04 -0500

    modificar

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * main - the function gives 10 times the lowercase alphabet.
+ * print_alphabet_x10 - the function gives 10 times the lowercase alphabet.
  *
- * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-    int a = 0;
+	int a = 0;
 
-    while (a < 10)
-    {
-        char letra;
+	while (a < 10)
+	{
+		char letra;
 
-        for (letra = 'a'; letra <= 'z'; letra++)
-        {
-            _putchar(letra);
-        }
-        _putchar('\n');
+		for (letra = 'a'; letra <= 'z'; letra++)
+		{
+			_putchar (letra);
+		}
+
+		_putchar('\n');
 		a++;
-     }
+	}
 }
