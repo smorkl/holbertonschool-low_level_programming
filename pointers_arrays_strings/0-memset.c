7244dc6cb95ad5f3a3c264ed0abef52375df67bd
smorkl <smsalazar0319@gmail.com>
2024-03-19 15:21:59 -0500

    create

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - The function will fill the spaces n of
+ *           the pointer s with the char b
+ * @s: the pointer pointing to the string
+ * @b: the char that will fill the spaces
+ * @n: number of spaces to fill
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int re = 0;
+
+	while (re < n)
+	{
+		s[re] = b;
+		re++;
+	}
+}
