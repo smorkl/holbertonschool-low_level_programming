8c9546b0dd2acc70b59763fff48b9fbe38f9726d
smorkl <smsalazar0319@gmail.com>
2024-03-19 07:26:43 -0500

    create

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - the fuction convert a string from lowercase to uppercase
+ * @ptr: the string
+ * Return: pointer with capital letters
+ */
+
+char *cap_string(char *ptr)
+{
+	int leng = 0;
+
+	while (ptr[leng])
+	{
+		if (ptr[leng] >= 'a' && ptr[leng] <= 'z')
+		{
+			ptr[leng] = (ptr[leng] - 32);
+		}
+		else
+		{
+			ptr[leng] = ptr[leng]
+		}
+		leng++;
+	}
+	return (ptr);
+}
