a77e865ab3d4b8854b86a6bfa63ebc2b7808f412
smorkl <smsalazar0319@gmail.com>
2024-03-20 12:13:55 -0500

    create

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the fuction print number of arguments of the array.
+ * @argc: the number of arguments
+ * @argv: an array of pointer to the arguments.
+ *
+ */
+
+int main(int argc, char argv[])
+{
+	(void)argc;
+	printf("%s\n", argc);
+
+	return (0);
+}
