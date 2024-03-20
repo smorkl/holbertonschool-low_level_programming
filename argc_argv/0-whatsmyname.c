70b2bf162ea8067b7526b5a1969a5d9616ed7c26
smorkl <smsalazar0319@gmail.com>
2024-03-20 12:03:05 -0500

    create

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - the fuction print the name of touch.
+ * @argc: the number of arguments
+ * @argv: an array of pointers to the arguments.
+ *
+ * Return: 0
+ */
+
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
