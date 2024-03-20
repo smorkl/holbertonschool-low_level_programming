ffe032ae5ad087a80fdebfb3735a7bdc4cb915ce
smorkl <smsalazar0319@gmail.com>
2024-03-20 14:13:51 -0500

    modificar error

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the fuction print the multiply 2 numbers from the array.
+ * @argc: the number of arguments
+ * @argv: the array of pointers to arguments.
+ *
+ * Return: return 0 if multiply two numbers or 1 if argc < 2.
+ */
+
+int main(int argc, char *argv[])
+{
+	int resultado;
+	int num1, num2;
+
+	if (argc < 3)
+	{
+		printf("error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	resultado = num1 * num2;
+	printf("%d\n", resultado);
+
+	return (o);
+}
