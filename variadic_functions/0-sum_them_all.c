a871d440f706529e7e4f3605d658111676eed902
smorkl <smsalazar0319@gmail.com>
2024-03-21 12:11:01 -0500

    create

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int resultado = 0;
+	int cconteo;
+	va_list args;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(args, n);
+	for (conteo = 0; conteo < n; conteo++)
+	{
+		resultado += va_args, int;
+	}
+	va_end(args);
+	return (resultado);
+}
