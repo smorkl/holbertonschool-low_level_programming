24294b5f6f81577f564ff18e7ba0969151f7603c
smorkl <smsalazar0319@gmail.com>
2024-03-21 13:52:40 -0500

    create

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - the fuction print strings follow of a separator.
+ * @n: the number of strings.
+ * @separator: the string that separates the strings.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list cadenas;
+	unsigned int contador;
+	char *cont;
+
+	va_start(arg, n);
+	for (contador = 0; contador < n; contador++)
+	{
+		cont = va_arg(cadenas, char *);
+
+		if (cont == NULL)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%s", cont[0]);
+		}
+
+		if (separator != NULL && contador != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(args);
+}
