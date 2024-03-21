7a6da0e552d6f6518f49e694fe6966cf0bf0884b
smorkl <smsalazar0319@gmail.com>
2024-03-21 14:02:32 -0500

    create

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%d", cont[0]);
+			printf("%s", cont[contador]);
 		}
 
 		if (separator != NULL && contador != (n - 1))
