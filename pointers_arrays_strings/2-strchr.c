4bb977ce7b4a7576e138f47c03d46982ec13f05a
smorkl <smsalazar0319@gmail.com>
2024-03-26 00:00:36 -0500

    create

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr -
+ * @s:
+ * @c:
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int re = 0;
+	char *s2;
+
+	while (s[re])
+	{
+		if (s[re] == c)
+		{
+			return (s2)
+		}
+		s2[re] = s[re];
+		re++;
+	}
+}
