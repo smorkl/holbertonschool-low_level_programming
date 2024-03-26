e0c0f56eff2d42a8c4f61673a74de2448646db8e
smorkl <smsalazar0319@gmail.com>
2024-03-26 00:10:50 -0500

    create

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strchr -
- * @s:
- * @c:
+ * _strchr - the fuction find a char.
+ * @s: the pointer of chars.
+ * @c: the char to look for.
  *
- * Return:
+ * Return: if the fuction found char c return char c
  */
 
 char *_strchr(char *s, char c)
