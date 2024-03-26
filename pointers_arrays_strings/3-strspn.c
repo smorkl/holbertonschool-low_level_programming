19f75f71890816a2b3ba4cf569081f6d6e822324
smorkl <smsalazar0319@gmail.com>
2024-03-26 07:51:37 -0500

    create

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include <strdio.h>
+#include "main.h"
+
+/**
+ * _strspn - the futcion la funcion suma cuantas
+ *           veces se encuntra un char en un string
+ * @s: the string of the char.
+ * @accept: the char string to find
+ *
+ */
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	unsigned int find;
+	int pasar = 0;
+
+	while (s[pasar])
+	{
+		for (find = 0; accept[find]; find++)
+		{
+			if (s[pasar] == accept[find])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[find + 1] == '\0')
+				return (bytes);
+		}
+
+		pasar++;
+	}
+
+	return (bytes);
+}
