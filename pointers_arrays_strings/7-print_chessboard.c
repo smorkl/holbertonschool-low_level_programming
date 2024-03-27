e894502d3ce902f170191474dbc571198302d7a7
smorkl <smsalazar0319@gmail.com>
2024-03-27 11:40:15 -0500

    create

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - the fuction prints a chessboard
+ * @a: the chessborad.
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+
+	while (i < 8)
+	{
+		int j = 0;
+
+		while (j < 8)
+		{
+			printf("%c", a[i][j]);
+			j++;
+		}
+		printf ("\n");
+		i++;
+	}
+}
