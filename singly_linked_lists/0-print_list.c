8fb089bfee2ffb2c87c79d8c5ac15c1778bd9ef3
smorkl <smsalazar0319@gmail.com>
2024-04-01 18:15:58 -0500

    modificar

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - the fuction print all the elements of a lists_t.
+ * @h: the list_t.
+ *
+ * Return: the number of nodes.
+ */
+
 size_t print_list(const list_t *h)
 {
+	size_t nodes;
 
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("%s\n", h->str);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
