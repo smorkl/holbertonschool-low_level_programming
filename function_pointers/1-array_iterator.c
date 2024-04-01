1c38ad80610c4c6add3c8e0eb94840b0f8b0bb8d
smorkl <smsalazar0319@gmail.com>
2024-04-01 10:25:31 -0500

    create

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers"
+
+/**
+ * array_iterator - the function executes a function given
+ *                  as a parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
