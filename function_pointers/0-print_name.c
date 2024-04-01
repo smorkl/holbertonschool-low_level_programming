a6d4c5b677ed0a0ba2f992b407c40dff81611ab1
smorkl <smsalazar0319@gmail.com>
2024-04-01 10:09:29 -0500

    create

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+}
