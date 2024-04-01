377cdd9785ffb0203f062ff90af1366218eb3fb4
smorkl <smsalazar0319@gmail.com>
2024-03-31 22:13:48 -0500

    modificar

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * _puts_recursion - the fuction print a string.
+ * @s: a string.
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
