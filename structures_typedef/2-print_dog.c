04e5d5715356811e1ca68165e0755df791cc4530
smorkl <smsalazar0319@gmail.com>
2024-03-31 23:35:45 -0500

    create

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - the function prints the dog structure
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+		printf("name: %s\n", d->name);
+
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+		printf("Name: %f\n", d->age);
+
+	if (d->ownwe == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+		printf("Name: %s\n", d->owner);
+}
