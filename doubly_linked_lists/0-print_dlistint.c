eca939f707368e5c1cd411da067ee2b98bbe367b
smorkl <smsalazar0319@gmail.com>
2024-04-06 20:17:07 -0500

    create

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_dlistint - the fuction print all the element.
+ * @h: a poonter to the head.
+ *
+ * Return: return number of the nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int cant_nodes = 0;
+
+	while (h)
+	{
+		cant_nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (cant_nodes);
+}
