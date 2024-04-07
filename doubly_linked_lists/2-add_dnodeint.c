fef0155b7caf8c44bf4613e5e3f3415f164b2698
smorkl <smsalazar0319@gmail.com>
2024-04-06 21:02:06 -0500

    create

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -22,6 +22,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = *head;
+	new->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
 
 	*head = new;
 
