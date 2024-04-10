661522ba56d1717ce1e740c6d2cc3ed8c53435e5
smorkl <smsalazar0319@gmail.com>
2024-04-09 19:55:50 -0500

    create

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,6 +5,8 @@
  *                            indicated position
  * @head: a pointer to the head pointer.
  * @index: node position
+ *
+ * Return: upon success -1, otherwise 1.
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
