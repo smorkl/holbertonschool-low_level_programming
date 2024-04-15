1ffb21e8150474df820f6ed7dc2ea02d6d26fec8
smorkl <smsalazar0319@gmail.com>
2024-04-15 00:00:33 -0500

    create

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -8,16 +8,16 @@
  *
  * Return: The number of bits needed to flip.
  */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = n ^ m;
-    unsigned int count = 0;
+	unsigned long int xor = n ^ m, bits = 0;
 
-    while (xor_result != 0)
-    {
-        count += xor_result & 1;
-        xor_result >>= 1;
-    }
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
 
-    return (count);
+	return (bits);
 }
