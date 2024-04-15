38d7d25ab87ce6ec2ac00ac91417b9dd37b8f661
smorkl <smsalazar0319@gmail.com>
2024-04-14 21:26:52 -0500

    modificar

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - the function copy a string of characters
+ * @dest: string of destination
+ * @src: string of char
+ * @n: number of n bytes
+ *
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int contador_bytes = 0;
+
+	while (contador_bytes < n && src[contador_bytes] != '\0')
+	{
+		dest[contador_bytes] = src[contador_bytes];
+		contador_bytes++;
+	}
+
+	while (contador_bytes < n)
+	{
+		dest[contador_bytes] = '\0';
+		contador_bytes++;
+	}
+	return (dest);
+}
