3197440aee5cd1aca31c177519776663f16097de
smorkl <smsalazar0319@gmail.com>
2024-04-22 19:29:02 -0500

    modificar

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,13 +1,17 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
  * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * @filename: A pointer to the name
+ * @text_content: A pointer to a string
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
